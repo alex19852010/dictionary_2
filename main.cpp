3a8418f633440159792812f2c5e6fda3d848e8ab
Alex Bogushenko <bogushenkoas@ukr.net>
2023-05-08 12:49:06 +0300

    2 commit

--- main.cpp
+++ main.cpp
@@ -1,6 +1,35 @@
 #include <iostream>
+#include <string>
+#include <map>
+
+using namespace std;
+
+map < string, int > registry;
+string surname;
+int number = 1;
+int count = 5;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    while(count != 0)
+    {
+        cout << "add surname:" ;
+        cin >> surname;
+        registry.insert(pair < string, int >( surname, number));
+        number ++;
+        count --;
+    }
+
+    int val = 5;
+    while(val != 0)
+
+   {
+       auto it = registry.begin();
+        cout << registry.begin()->first << endl;
+        registry.erase(it);
+        val --;
+   }
+
+
+
     return 0;
 }
