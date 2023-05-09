a03c816b00b409d3cc1e5b5c924058df093b8c92
Alex Bogushenko <bogushenkoas@ukr.net>
2023-05-09 12:46:48 +0300

    3 commit

--- main.cpp
+++ main.cpp
@@ -4,32 +4,47 @@
 
 using namespace std;
 
-map < string, int > registry;
+map<string, int> registry;
 string surname;
 int number = 1;
-int count = 5;
+string calling;
 
 int main() {
-    while(count != 0)
-    {
-        cout << "add surname:" ;
+    while (1) {
+        cout << "add surname:";
         cin >> surname;
-        registry.insert(pair < string, int >( surname, number));
-        number ++;
-        count --;
+        registry.insert(pair<string, int>(surname, number));
+        number++;
+
+        auto it = registry.find(surname);
+
+        cout << "if you want to calling a person then click next:";
+        cin >> calling;
+        if(calling == "next")
+        {
+            for (it = registry.begin(); it != registry.end(); it++) {
+                cout << it->first << endl;
+            }
+
+            while( !(registry.empty()) )
+            {
+                auto m = registry.begin();
+                cout << "next" << endl;
+                cout << registry.begin()->first << endl;
+                registry.erase(m);
+            }
+
+
+        }
+//        else
+//        {
+//            for (it = registry.begin(); it != registry.end(); it++) {
+//                cout << it->first << endl;
+//            }
+//        }
     }
 
-    int val = 5;
-    while(val != 0)
-
-   {
-       auto it = registry.begin();
-        cout << registry.begin()->first << endl;
-        registry.erase(it);
-        val --;
-   }
+        return 0;
+    }
 
 
-
-    return 0;
-}
