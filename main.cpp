b3f739d3c9f0e7f398b735061d9eae9f3707268e
Alex Bogushenko <bogushenkoas@ukr.net>
2023-05-12 15:44:27 +0300

    6 comit

--- main.cpp
+++ main.cpp
@@ -4,8 +4,7 @@
 
 using namespace std;
 
-map<string, int> registry;
-map<int, string> reg;
+map<string, int> surnamecount;
 string surname;
 int number = 1;
 string calling;
@@ -15,65 +14,55 @@ int count = 0;
 
 int main() {
     while (1) {
+
         cout << "add surname:";
         cin >> surname;
-        registry.insert(pair<string, int>(surname, number));
-        reg.insert(pair<int, string>(number, surname));
-        number++;
 
-        auto it = registry.find(surname);
-        auto its = reg.find(number);
+        auto it = surnamecount.find(surname);
 
-        cout << "if you want to calling a person then click next:";
-        cin >> calling;
-        if (calling == "next") {
-            for (it = registry.begin(); it != registry.end(); it++) {
-                cout << it->first << endl;
-                str = it->first;
-                for (its = reg.begin(); its != reg.end(); its++) {
-                    if (str == its->second) {
-                        count++;
-                        if (str == its->second && count > 1) {
-                            cout << its->second << endl;
-                            count--;
-                        }
-
-                    }
-
-                }
-                count = 0;
-            }
-
-            while (!(registry.empty())) {
-
-                str = registry.begin()->first;
-                for (its = reg.begin(); its != reg.end(); its++) {
-                    if (str == its->second) {
-                        count++;
-
-
-                    }
-
-                }
-
-                for (int i = 0; i < count; i++) {
-                    cout << "next" << endl;
-                    cout << registry.begin()->first << endl;
-
-                }
-                count = 0;
-
-
-                registry.erase(str);
-
-
-            }
-
+        if (it == surnamecount.end()) {
+            surnamecount.insert(pair<string, int>(surname, number));
+        } else {
+            
+            it->second ++;
 
         }
 
+        cout << "if you want to calling a person then click next:";
+        cin >> calling;
+
+        if (calling == "next") {
+
+            it = surnamecount.begin();
+            str = it->first;
+            cout << "next" << endl;
+            cout << it->first << endl;
+            if(it->second == 1) {
+                
+                surnamecount.erase(str);
+            }
+            else{
+                  it->second --;
+                
+            }
+            
+        }
+        else{
+            for(it = surnamecount.begin(); it != surnamecount.end(); it ++)
+                {
+                
+                    int n = it->second;
+                    for(int i = 0; i < n; i ++)
+                    {
+                        cout << it->first << endl;
+                    }
+                }
+        }
+
+    
 
     }
+
 
 
     return 0;
